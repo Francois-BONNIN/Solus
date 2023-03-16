333980c21cd2da50d7bf39951a6e84cbf3e9d392
Francois BONNIN <francois.bonnin@ynov.com>
2023-03-16 17:37:00 +0100

    initialisation

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <App />
+        </MantineProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
