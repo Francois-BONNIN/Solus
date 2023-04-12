e7947863acaa435058ef79f30ed84dd5f37be19e
Francois BONNIN <francois.bonnin@ynov.com>
2023-04-12 14:31:21 +0200

    fix router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../views/Home";
-import { ActivityId } from "../views/ActivityID";
+import { ActivityId } from "../views/ActivityId";
 
 export const AppRouter = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="home" element={<Home />} />
-      <Route path="activities/:id" element={<ActivityId/>} />
+      <Route path="activities/:id" element={<ActivityId />} />
       <Route path="equipments/:id" element={<Home />} />
       <Route path="profile" element={<Home />} />
       <Route path="about-us" element={<Home />} />
